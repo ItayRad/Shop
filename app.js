62b7680ccd5143f276d77b3b0bfae1a23ecac8b5
itayr11 <itayrado111@gmail.com>
2020-02-07 00:46:01 +0200

    fix port

--- app.js
+++ app.js
@@ -343,8 +343,7 @@ else{
 }
 
 
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+var server = app.listen(process.env.PORT || 3000, function () {
+  var port = server.address().port;
+  console.log("Express is working on port " + port);
 });
