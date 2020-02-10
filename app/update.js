637d50f4f3e7bfe96b4f74d44be196a47de7b2e3
itayr11 <itayrado111@gmail.com>
2020-02-10 20:39:34 +0200

    v4

--- app/update.js
+++ app/update.js
@@ -10,15 +10,15 @@ router.post('/', function(req, res) {
   const oldName = req.body.oldName;
   const name = req.body.newName;
   //const newName = name.charAt(0).toUpperCase() + name.slice(1); // in register it saves only NOT capitalized
-  const newName = name.toLowerCase();
-  const newPassword = req.body.newPassword;
+  // const newName = name.toLowerCase();
+  //const newPassword = req.body.newPassword;
   const newPermission = req.body.newPermission;
-if (req.body.newPassword == "")
-{
+// if (req.body.newPassword == "")
+// {
   User.findOneAndUpdate({
     username: oldName
   }, {
-    username: newName,
+    //username: newName,
       //  password: newPassword, need to find way to modify password and encrypt it
     admin:newPermission
   }, function(err) {
@@ -30,24 +30,24 @@ if (req.body.newPassword == "")
       res.redirect("/");
     }
   });
-}
-else{
-  User.findOneAndUpdate({
-    username: oldName
-  }, {
-    username: newName,
-    //  password: newPassword, need to find way to modify password and encrypt it
-    admin:newPermission
-  }, function(err) {
-    if (err) {
-      res.render("failed.ejs", {
-        error: "Username is Taken, Please choose Another Name"
-      });
-    } else {
-      res.redirect("/");
-    }
-  });
-}
+// }
+// // else{
+//   User.findOneAndUpdate({
+//     username: oldName
+//   }, {
+//     username: newName,
+//     //  password: newPassword, need to find way to modify password and encrypt it
+//     admin:newPermission
+//   }, function(err) {
+//     if (err) {
+//       res.render("failed.ejs", {
+//         error: "Username is Taken, Please choose Another Name"
+//       });
+//     } else {
+//       res.redirect("/");
+//     }
+//   });
+// // }
 });
 
 module.exports = router;
