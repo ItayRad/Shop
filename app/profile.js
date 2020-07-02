dc296ae666c5e7cd02b2af947631487ae5aa0e41
itayr11 <itayrado111@gmail.com>
2020-07-02 22:29:05 +0300

    v3

--- app/profile.js
+++ app/profile.js
@@ -8,12 +8,14 @@ const Cart = require("./models/cart.js");
 router.get("/", function(req,res) {
 Order.find({user:req.user}, function(err,orders){
   if (err) {
-    return res.write("error finding orders");
+    return res.write("Error finding orders");
   }
   var cart;
+
   orders.forEach(function(order){
     cart = new Cart(order.cart);
-    order.items = cart.generateArray();
+    order.items = cart.generateArray();    // change cart information into array
+    order.coupons = cart.generateCouponArray();
   });
 
   res.render("profile", {title:"My Profile", user:req.user, orders:orders, req:req});
